9a806f5bc5e0058804c55f2db686e076abc8c8e0
Bhuvaneswari Babu <bhuvanamouttou@gmail.com>
2020-06-04 13:47:33 +0200

    Updates

---
+++ precache-manifest.7c58b0fffef2fdcb7e00753db6a908fe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676786d9a67c485d24a56a4ce47c960f",
+    "url": "/redux-robofriends/index.html"
+  },
+  {
+    "revision": "9caaa32cc8afdac48259",
+    "url": "/redux-robofriends/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a92a3cac49965d9afda2",
+    "url": "/redux-robofriends/static/css/main.88db1e39.chunk.css"
+  },
+  {
+    "revision": "9caaa32cc8afdac48259",
+    "url": "/redux-robofriends/static/js/2.e0ebee44.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/redux-robofriends/static/js/2.e0ebee44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92a3cac49965d9afda2",
+    "url": "/redux-robofriends/static/js/main.da43993a.chunk.js"
+  },
+  {
+    "revision": "daa8cff206ade59f9240",
+    "url": "/redux-robofriends/static/js/runtime-main.79afdf1c.js"
+  },
+  {
+    "revision": "3f5c9111609279eb69be9e1a8a10e82b",
+    "url": "/redux-robofriends/static/media/SEGA-LOGO-FONT.ttf.3f5c9111.woff"
+  },
+  {
+    "revision": "57ad6cb1828e85f5f029d166ac0361b8",
+    "url": "/redux-robofriends/static/media/SEGA-LOGO-FONT.ttf.57ad6cb1.eot"
+  },
+  {
+    "revision": "d7d67c411a56f01384056ff8a85eb4c6",
+    "url": "/redux-robofriends/static/media/SEGA-LOGO-FONT.ttf.d7d67c41.svg"
+  }
+]);
